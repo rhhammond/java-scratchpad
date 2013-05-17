8ae74960677f6f346b8dc488b4239a125736bbf0
Rob Hammond <rhammond@homeaway.com>
2013-05-17 01:41:00 -0500

    added static initializer blocks to Bottom.java

--- scjp/ctors/Bottom.java
+++ scjp/ctors/Bottom.java
@@ -1,19 +1,24 @@
 /* Static initializer blocks are executed before ctors! */
 
 class Top {
+	static { System.out.println("Static Top initializer"); }
 	{ System.out.println("Top initializer"); }
 	public Top(){ System.out.println("Top ctor"); }
 }
 
 class Middle extends Top {
+	static { System.out.println("Static Middle initializer"); }
 	{ System.out.println("Middle initializer"); }
 	public Middle(){ System.out.println("Middle ctor");}
 }
 
 public class Bottom extends Middle {
+	static { System.out.println("Static Bottom initializer"); }
 	{ System.out.println("Bottom initializer"); }
 	public Bottom(){ System.out.println("Bottom ctor"); }
 	public static void main(String[] args) {
+		new Bottom();
+		System.out.println("\nRound 2!\n");
 		new Bottom();
 	}
 }
