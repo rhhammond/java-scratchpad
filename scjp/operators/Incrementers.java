08078446b54552ea919d7b77d44c1956772e851c
Rob Hammond <robert.h.hammond@gmail.com>
2013-05-19 15:32:48 -0500

    added Incrementers.java

---
+++ scjp/operators/Incrementers.java
@@ -0,0 +1,13 @@
+public class Incrementers {
+
+	public static void main(String[] args) {
+		int x =0;
+		
+		if(x++ == 1) System.out.println(true); 
+		else System.out.println(false);
+		
+		if(++x == 2) System.out.println(true); 
+		else System.out.println(false);	
+	}
+
+}
